a9c741014a8bca49aa0550eb7f076e6e971f140e
S-000 <putiienko.kiev@gmail.com>
2019-12-13 14:54:19 +0100

    Search Users

--- app/src/main/java/com/example/app/UsersFragment.java
+++ app/src/main/java/com/example/app/UsersFragment.java
@@ -1,16 +1,24 @@
 package com.example.app;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -33,6 +41,10 @@ public class UsersFragment extends Fragment {
     AdapterUsers adapterUsers;
     List<ModelUser> userList;
 
+
+
+    FirebaseAuth firebaseAuth;
+
     // com.android.support.recyclerview
     public UsersFragment() {
         // Required empty public constructor
@@ -46,6 +58,8 @@ public class UsersFragment extends Fragment {
 
         View view =  inflater.inflate(R.layout.fragment_users, container, false);
 
+
+        firebaseAuth = FirebaseAuth.getInstance();
         //init recyclerview
 
         recyclerView = view.findViewById(R.id.users_recyclerView);
@@ -95,6 +109,121 @@ public class UsersFragment extends Fragment {
 
             }
         });
+    }
+
+    private void searchUsers(final String query) {
+        //get current user
+        final FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+        //get path of database named "Users" containing users info
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        //get all data from path
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userList.clear();
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    ModelUser modelUser = ds.getValue(ModelUser.class);
+
+                    //get all searched users except currently signed in user
+                    if(!modelUser.getUid().equals(fUser.getUid())){
+                        if(modelUser.getName().toLowerCase().contains(query.toLowerCase()) ||
+                        modelUser.getEmail().toLowerCase().contains(query.toLowerCase())){
+                            userList.add(modelUser);
+                        }
+
+
+                    }
+                    //adapter
+                    adapterUsers = new AdapterUsers(getActivity(), userList);
+                    //refresh adapter
+                    adapterUsers.notifyDataSetChanged();
+                    //set adapter to recycler view
+                    recyclerView.setAdapter(adapterUsers);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void checkUserStatus(){
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null){
+            //user is signed in stay here
+            //set email of logged in user
+            //
+
+        }
+        else {
+            // user not signed in, go to main activity
+            startActivity(new Intent(getActivity(),MainActivity.class));
+            getActivity().finish();
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        setHasOptionsMenu(true); //to show menu option in fragment
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main,menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                if(!TextUtils.isEmpty(s.trim())){
+                    searchUsers(s);
+                }
+                else{
+                    getAllUsers();
+                }
+                return false;
+            }
+
+
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                if(!TextUtils.isEmpty(s.trim())){
+                    searchUsers(s);
+                }
+                else{
+                    getAllUsers();
+                }
+                
+                return false;
+            }
+        });
+
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+
+    //handle menu item clicks
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //get item id
+        int id = item.getItemId();
+        if(id == R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
