9fad72e2a17fef0ffadfca96c8ff73a56a22417b
S-000 <putiienko.kiev@gmail.com>
2019-12-13 16:47:15 +0100

    new implementations

--- app/src/main/java/com/example/app/UsersFragment.java
+++ app/src/main/java/com/example/app/UsersFragment.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
