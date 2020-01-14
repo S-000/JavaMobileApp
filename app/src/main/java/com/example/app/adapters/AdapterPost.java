9b372ea9b4dc46260989aeb2232b64b58098399d
S-000 <putiienko.kiev@gmail.com>
2020-01-15 00:08:29 +0200

    visual for post

---
+++ app/src/main/java/com/example/app/adapters/AdapterPost.java
@@ -0,0 +1,107 @@
+package com.example.app.adapters;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.app.R;
+import com.example.app.models.ModelPost;
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class AdapterPost extends RecyclerView.Adapter<AdapterPost.MyHolder> {
+
+    Context context;
+    List<ModelPost> postList;
+
+    public AdapterPost(Context context, List<ModelPost> postList) {
+        this.context = context;
+        this.postList = postList;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+//        View view = LayoutInflater.from(context).inflate(R.layout.row_posts, viewGroup, false)
+        View view = LayoutInflater.from(context).inflate(R.layout.row_posts, parent, false);
+
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+        String uid = postList.get(position).getUid();
+        String uEmail = postList.get(position).getuEmail();
+        String uName = postList.get(position).getuName();
+        String uDp = postList.get(position).getuDp();
+        String pId = postList.get(position).getpId();
+        String pTitle = postList.get(position).getpTitle();
+        String pDescr = postList.get(position).getpDescr();
+        String pImage = postList.get(position).getpImage();
+        String pTimeStamp = postList.get(position).getpTime();
+
+        Calendar calendar = Calendar.getInstance(Locale.getDefault());
+        calendar.setTimeInMillis(Long.parseLong(pTimeStamp));
+        String pTime = (String) DateFormat.format("dd/MM/yyyy hh:mm:ss", calendar);
+
+        holder.uNameTv.setText(uName);
+        holder.pTimeTv.setText(pTime);
+        holder.pTitleTv.setText(pTime);
+
+        try{
+            Picasso.get().load(uDp).placeholder(R.drawable.ic_default_img).into(holder.uPictureIv);
+        }
+        catch (Exception e){
+
+        }
+        try {
+            Picasso.get().load(pImage).into(holder.pImageIv);
+        }
+        catch (Exception e){
+
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    class  MyHolder extends RecyclerView.ViewHolder{
+        ImageView uPictureIv, pImageIv;
+        TextView uNameTv, pTimeTv, pDescriptionTv, pLikesTv;
+        ImageButton moreBtn;
+        Button likeBtn, commentBtn, shareBtn;
+
+        public MyHolder(View itemView){
+            super(itemView);
+            uPictureIv = itemView.findViewById(R.id.uPictureIv);
+            pImageIv = itemView.findViewById(R.id.pImageIv);
+            uNameTv = itemView.findViewById(R.id.uNameTv);
+            pTimeTv = itemView.findViewById(R.id.pTimeTv);
+            pDescriptionTv = itemView.findViewById(R.id.pDescriptionTv);
+            pLikesTv = itemView.findViewById(R.id.pLikesTv);
+            pLikesTv = itemView.findViewById(R.id.pLikesTv);
+            moreBtn = itemView.findViewById(R.id.moreBtn);
+            likeBtn = itemView.findViewById(R.id.likeBtn);
+            commentBtn = itemView.findViewById(R.id.commentBtn);
+            shareBtn = itemView.findViewById(R.id.shareBtn);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/app/models/ModelPost.java
@@ -0,0 +1,102 @@
+package com.example.app.models;
+
+public class ModelPost {
+    String pId;
+    String pTitle;
+    String pDescr;
+    String pImage;
+    String uid;
+    String pTime;
+    String uDp;
+    String uName;
+
+    public String getuEmail() {
+        return uEmail;
+    }
+
+    public void setuEmail(String uEmail) {
+        this.uEmail = uEmail;
+    }
+
+    String uEmail;
+
+    public ModelPost(){
+
+    }
+
+    public ModelPost(String pId, String uEmail, String pTitle, String pDescr, String pImage, String uid, String pTime, String uDp, String uName) {
+        this.pId = pId;
+        this.pTitle = pTitle;
+        this.pDescr = pDescr;
+        this.pImage = pImage;
+        this.uid = uid;
+        this.pTime = pTime;
+        this.uDp = uDp;
+        this.uName = uName;
+        this.uEmail = uEmail;
+    }
+
+    public String getpId() {
+        return pId;
+    }
+
+    public void setpId(String pId) {
+        this.pId = pId;
+    }
+
+    public String getpTitle() {
+        return pTitle;
+    }
+
+    public void setpTitle(String pTitle) {
+        this.pTitle = pTitle;
+    }
+
+    public String getpDescr() {
+        return pDescr;
+    }
+
+    public void setpDescr(String pDescr) {
+        this.pDescr = pDescr;
+    }
+
+    public String getpImage() {
+        return pImage;
+    }
+
+    public void setpImage(String pImage) {
+        this.pImage = pImage;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getpTime() {
+        return pTime;
+    }
+
+    public void setpTime(String pTime) {
+        this.pTime = pTime;
+    }
+
+    public String getuDp() {
+        return uDp;
+    }
+
+    public void setuDp(String uDp) {
+        this.uDp = uDp;
+    }
+
+    public String getuName() {
+        return uName;
+    }
+
+    public void setuName(String uName) {
+        this.uName = uName;
+    }
+}
